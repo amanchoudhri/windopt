8b4a8a940076abeab4c9ba74d4626b1c2e9d9994
Aman Choudhri <amanchoudhri@gmail.com>
2025-01-09 20:42:28 -0500

    Update power convergence figure for new initial LES runs

--- src/windopt/viz/power_convergence.py
+++ src/windopt/viz/power_convergence.py
@@ -2,12 +2,15 @@
 Plot power convergence over time of wind farm simulations.
 """
 
+from argparse import ArgumentParser
 from pathlib import Path
+from typing import Optional
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from windopt.constants import PROJECT_ROOT
+from windopt.constants import PROJECT_ROOT, DT
 from windopt.winc3d.io import turbine_results
 
 DEFAULT_N_TIMESTEPS_PER_FILE = 9000
@@ -17,86 +20,98 @@ def power_trajectory(
         job_dir: Path,
         n_timesteps_per_file: int,
         spinup_timesteps: int,
+        dt: float = DT
     ) -> pd.DataFrame:
     """
     Get the power trajectory (in MW) of a LES simulation from its job directory.
     """
     power_df = turbine_results(job_dir)
+
     # Throw out spinup timesteps
     spinup_filenumbers = int(spinup_timesteps / n_timesteps_per_file)
     power_df = power_df[power_df['filenumber'] > spinup_filenumbers]
     power_trajectory = power_df.groupby("filenumber")[["Power", "Power_ave"]].sum().reset_index()
+
     # Calculate average power per timestep
     power_trajectory['Power_ave'] = power_trajectory['Power_ave'] / (
         (1 + power_trajectory['filenumber'] * n_timesteps_per_file) - spinup_timesteps)
+
     # convert to MW
     power_trajectory['Power_ave'] = power_trajectory['Power_ave'] / 1e6
     power_trajectory['Power'] = power_trajectory['Power'] / 1e6
+
+    # convert the filenumber to time after spinup, in hours
+    files_after_spinup = power_trajectory['filenumber'] - spinup_filenumbers
+    seconds_after_spinup = files_after_spinup * n_timesteps_per_file * dt
+    power_trajectory['time_hr'] = seconds_after_spinup / 3600.0
+
     return power_trajectory
 
-def validate_power_convergence():
+def plot_trajectory(
+        ax: plt.Axes,
+        trajectory_df: pd.DataFrame,
+        title: Optional[str] = None,
+        scatter_alpha: float = 0.4,
+        line_alpha: float = 0.8
+    ):
+    sns.scatterplot(
+        data=trajectory_df,
+        x="time_hr",
+        y="Power",
+        alpha=scatter_alpha,
+        ax=ax
+    )
+    sns.lineplot(
+        data=trajectory_df,
+        x="time_hr",
+        y="Power_ave",
+        alpha=line_alpha,
+        ax=ax
+    )
+    ax.set_xlabel('Time (hr)')
+    ax.set_ylabel('Power (MW)')
+
+    if title:
+        ax.set_title(title)
+
+    ax.minorticks_on()
+    ax.grid(which='minor', linestyle=':', alpha=0.3)
+
+def validate_grid_and_random(grid_dir: Path, random_dir: Path, outfile: Path):
     layouts = ("grid", "random")
 
-    simulation_dir = Path('/moto/edu/users/ac4972/validate_les')
+    sns.set_theme(style="whitegrid")
+    sns.set_context("talk")
 
-    power_trajectories = []
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5), sharex=True, sharey=True)
 
-    for layout in layouts:
-        for use_precursor in (True, False):
-            precursor_string = "" if use_precursor else "_no_precursor"
-            dirname = f"validate_les_{layout}{precursor_string}"
-            trajectory_df = power_trajectory(
-                simulation_dir / dirname,
-                n_timesteps_per_file=300,
-                spinup_timesteps=9000,
-            )
-            trajectory_df["layout"] = layout.capitalize()
-            trajectory_df["use_precursor"] = use_precursor
-            power_trajectories.append(trajectory_df)
-
-    power_trajectories = pd.concat(power_trajectories)
-
-    # subtract off the first 30 files representing the 30min spinup period
-    # where no measurements were recorded
-    power_trajectories['time_hr'] = (power_trajectories['filenumber'] - 30) / 60
-
-    # map True to "With Precursor" and False to "Without Precursor"
-    power_trajectories['precursor_display'] = power_trajectories['use_precursor'].map(
-        lambda x: "With Precursor" if x else "Without Precursor"
-    )
-
-    # facet by precursor usage
-    g = sns.FacetGrid(
-        data=power_trajectories,
-        col="precursor_display",
-        row="layout",
-        hue="layout"
+    for ax, layout, les_dir in zip(axs, layouts, (grid_dir, random_dir)):
+        trajectory_df = power_trajectory(
+            les_dir,
+            n_timesteps_per_file=300,
+            spinup_timesteps=9000,
         )
-    g.map(sns.scatterplot,
-        "time_hr", "Power",
-        alpha=0.4,
-        label='Sampled Instantaneous Power Output'
-        )
-    g.map(sns.lineplot,
-        "time_hr",
-        "Power_ave",
-        alpha=0.8,
-        label='Cumulative Average Power Output'
-        )
+        plot_trajectory(ax, trajectory_df, title=layout.capitalize())
 
-    g.set_xlabels("Time (hr)")
-    g.set_ylabels("Power (MW)")
-    g.set_titles(
-        "{row_name} Layout, {col_name}"
-        )
-
-    g.figure.suptitle('Simulated Power Output Throughout LES')
+    fig.suptitle('Simulated Power Output Throughout LES')
     plt.tight_layout()
 
-    # save the plot
-    plt.savefig(PROJECT_ROOT / "power_convergence.png", dpi=300)
+    # # save the plot
+    plt.savefig(outfile, dpi=300)
+
+def validate_grid_and_random_cli():
+    p = ArgumentParser()
+    p.add_argument('--grid_dir', type=Path, required=True)
+    p.add_argument('--random_dir', type=Path, required=True)
+    p.add_argument(
+        '--outfile',
+        type=Path,
+        default=PROJECT_ROOT / 'img' / 'power_convergence.png'
+        )
+    args = p.parse_args()
+    validate_grid_and_random(args.grid_dir, args.random_dir, args.outfile)
 
 
 if __name__ == "__main__":
-    validate_power_convergence()
+    validate_grid_and_random_cli()
 
