0ed38217dd5f21b6fa0496a33e5bfff78078bf77
Aman Choudhri <amanchoudhri@gmail.com>
2025-01-11 18:50:57 -0500

    Create new one-off scripts directory

--- scripts/validate_les.py
+++ scripts/validate_les.py
@@ -4,12 +4,11 @@ Queue a LES job with very frequent visualization and statistics outputs.
 Explores the convergence of the LES to the steady state.
 """
 
-from pathlib import Path
-
-import numpy as np
-
-from windopt.constants import PROJECT_ROOT, INFLOW_20M, INFLOW_20M_N_TIMESTEPS
-from windopt.winc3d.slurm import LESJob
+from windopt.constants import (
+    INFLOW_20M, INFLOW_20M_N_TIMESTEPS, PROJECT_ROOT, SMALL_BOX_DIMS,
+    VIZ_INTERVAL_FREQUENT
+)
+from windopt.winc3d.config import InflowConfig, LESConfig, OutputConfig, TurbineConfig
 from windopt.winc3d.les import start_les
 from windopt.layout import load_layout_batch, Layout
 
@@ -33,11 +32,14 @@ def load_layout(layout_name: str) -> Layout:
 if __name__ == "__main__":
     for layout_name in ["random", "grid"]:
         layout = load_layout(layout_name)
+        config = LESConfig(
+            box_dims=SMALL_BOX_DIMS,
+            turbines=TurbineConfig(layout=layout),
+            inflow=InflowConfig(directory=INFLOW_20M, n_timesteps=INFLOW_20M_N_TIMESTEPS),
+            output=OutputConfig(viz_interval=VIZ_INTERVAL_FREQUENT)
+        )
         job = start_les(
             run_name=f"validate_les_{layout_name}",
-            layout=layout,
-            inflow_directory=INFLOW_20M,
-            inflow_n_timesteps=INFLOW_20M_N_TIMESTEPS,
-            debug_mode=False,
-            frequent_viz=True,
+            config=config
         )
+        print(f"Queued LES, SLURM job id: {job.slurm_job_id}")
