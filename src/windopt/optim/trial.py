bd4b68207c115a44d252d3f6b0a08b131bca4230
Aman Choudhri <amanchoudhri@gmail.com>
2025-01-11 18:57:51 -0500

    Update trial.py to use LESConfig

--- src/windopt/optim/trial.py
+++ src/windopt/optim/trial.py
@@ -9,12 +9,15 @@ from ax.core.observation import ObservationFeatures
 from ax.service.ax_client import AxClient
 
 from windopt.constants import (
-    INFLOW_20M, INFLOW_20M_N_TIMESTEPS
+    INFLOW_20M, INFLOW_20M_N_TIMESTEPS,
+    N_STEPS_DEBUG, N_STEPS_PRODUCTION,
+    VIZ_INTERVAL_DEFAULT, VIZ_INTERVAL_FREQUENT
 )
 from windopt.gch import gch
 from windopt.optim.config import CampaignConfig
 from windopt.optim.utils import layout_from_ax_params
 from windopt.winc3d import start_les, process_results
+from windopt.winc3d.config import FlowConfig, InflowConfig, LESConfig, NumericalConfig, OutputConfig, TurbineConfig
 from windopt.winc3d.io import cleanup_viz_files
 from windopt.winc3d.slurm import LESJob
 
@@ -31,7 +34,7 @@ def complete_noiseless_trial(ax_client: AxClient, trial_index: int, power: float
         raw_data={'power': (power, 0.0)}
     )
 
-def run_gch_batch(ax_client: AxClient, batch_size: int, n_turbines: int):
+def run_gch_batch(ax_client: AxClient, campaign_config: CampaignConfig, batch_size: int):
     """
     Run a batch of GCH trials on the provided Ax client.
     """
@@ -42,7 +45,7 @@ def run_gch_batch(ax_client: AxClient, batch_size: int, n_turbines: int):
         )
     )
     for trial_index, parameters in trial_index_to_params.items():
-        layout = layout_from_ax_params(parameters, n_turbines)
+        layout = layout_from_ax_params(campaign_config, parameters)
         power = gch(layout).sum()
         complete_noiseless_trial(ax_client, trial_index, power)
 
@@ -66,14 +69,20 @@ def start_les_batch(
     jobs = []
     for trial_index, parameters in trial_index_to_params.items():
         layout = layout_from_ax_params(campaign_config, parameters)
+        n_steps = N_STEPS_PRODUCTION if not debug_mode else N_STEPS_DEBUG
+        viz_interval = VIZ_INTERVAL_DEFAULT if not debug_mode else VIZ_INTERVAL_FREQUENT
+        config = LESConfig(
+            box_dims=campaign_config.box_dims,
+            numerical=NumericalConfig(n_steps=n_steps),
+            output=OutputConfig(viz_interval=viz_interval),
+            turbines=TurbineConfig(layout=layout),
+            inflow=InflowConfig(directory=INFLOW_20M, n_timesteps=INFLOW_20M_N_TIMESTEPS)
+        )
         logger.info(f"Submitting LES job for trial {trial_index}")
         # Start LES job
         job = start_les(
             run_name=f"{campaign_config.name}_trial_{trial_index}",
-            layout=layout,
-            inflow_directory=INFLOW_20M,
-            inflow_n_timesteps=INFLOW_20M_N_TIMESTEPS,
-            debug_mode=debug_mode
+            config=config,
         )
         logger.info(f"Job submitted with ID: {job.slurm_job_id}")
         jobs.append((job, trial_index))
@@ -84,7 +93,6 @@ def process_completed_les_jobs(
         ax_client: AxClient,
         active_jobs: list[tuple[LESJob, int]],
         logger: logging.Logger,
-        debug_mode: bool,
         ):
     """
     Process completed LES jobs.
@@ -92,7 +100,8 @@ def process_completed_les_jobs(
     for job, trial_index in active_jobs[:]:
         if job.is_complete():
             try:
-                power = process_results(job, debug_mode=debug_mode)
+                config = LESConfig.from_json(job.job_dir / "les_config.json")
+                power = process_results(job, config)
                 complete_noiseless_trial(ax_client, trial_index, power)
             except Exception as e:
                 logger.error(f"Error processing results for trial {trial_index}: {e}")
