44b1d809585e7901de72e841811b6f78e6ac80cb
Aman Choudhri <amanchoudhri@gmail.com>
2025-01-11 19:01:24 -0500

    Add useful extra constants

--- src/windopt/constants.py
+++ src/windopt/constants.py
@@ -17,11 +17,17 @@ LARGE_ARENA_DIMS = (18 * D, 18 * D)
 SMALL_BOX_DIMS = (2004, 504, 1336)
 LARGE_BOX_DIMS = (4008, 504, 3340)
 
-# time step size for large eddy simulations
-DT = 0.2
-
 # mesh shape for large eddy simulations
 MESH_SHAPE_20M = (101, 51, 72) # 20m horizontal resolution and small arena dims
 
 INFLOW_20M = PROJECT_ROOT / 'simulations/small_arena_20m/planes'
 INFLOW_20M_N_TIMESTEPS = 6000
+
+# Simulation time parameters
+DT = 0.2  # Timestep in seconds
+N_STEPS_PRODUCTION = 45000  # 2.5 hours of simulation time
+N_STEPS_DEBUG = 2000  # Short run for testing
+
+# Visualization parameters 
+VIZ_INTERVAL_DEFAULT = 9000  # Default timesteps between outputs (30 minutes if DT = 0.2)
+VIZ_INTERVAL_FREQUENT = int(60 / DT)  # Output every simulated minute
